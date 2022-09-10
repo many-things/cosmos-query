857d9d02307cf5b979608323fcd3c6d5940b7b84
Dan <ryzond6728@gmail.com>
2022-09-11 03:36:02 +0900

    add applications queries

---
+++ packages/core/src/apis/ibc/applications/index.ts
@@ -0,0 +1,43 @@
+import { instance } from "../../../utils";
+import type { PaginationParams } from "../../cosmos";
+import type {
+  DenomTraceResponse,
+  DenomTracesResponse,
+  TransferParamsResponse,
+} from "./types";
+
+export type { DenomTraceResponse, DenomTracesResponse, TransferParamsResponse };
+
+export const getDenomTraces =
+  (baseURL: string) =>
+  async ({
+    pagination,
+  }: {
+    pagination?: PaginationParams;
+  }): Promise<DenomTracesResponse> => {
+    return (
+      await instance(baseURL).get(
+        "/ibc/applications/transfer/v1beta1/denom_traces",
+        {
+          params: { pagination },
+        }
+      )
+    ).data;
+  };
+
+export const getDenomTrace =
+  (baseURL: string) =>
+  async ({ hash }: { hash: string }): Promise<DenomTraceResponse> => {
+    return (
+      await instance(baseURL).get(
+        `/ibc/applications/transfer/v1beta1/denom_traces/${hash}`
+      )
+    ).data;
+  };
+
+export const getTransferParams =
+  (baseURL: string) => async (): Promise<TransferParamsResponse> => {
+    return (
+      await instance(baseURL).get("/ibc/applications/transfer/v1beta1/params")
+    ).data;
+  };
---
+++ packages/core/src/apis/ibc/applications/types.ts
@@ -0,0 +1,22 @@
+import type { PaginationResponse } from "../../../types";
+
+interface DenomTrace {
+  path: string;
+  base_denom: string;
+}
+
+export interface DenomTracesResponse {
+  denom_traces: DenomTrace[];
+  pagination: PaginationResponse;
+}
+
+export interface DenomTraceResponse {
+  denom_trace: DenomTrace;
+}
+
+export interface TransferParamsResponse {
+  params: {
+    send_enabled: boolean;
+    receive_enabled: boolean;
+  };
+}
