88168d2a048087e856c98cc78ff177930c5ef3a5
Dan <ryzond6728@gmail.com>
2022-09-21 17:00:36 +0900

    Add overview queries

---
+++ packages/core/src/apis/osmosisHistorical/overview/index.ts
@@ -0,0 +1,12 @@
+import { osmosisInstance } from "../../../utils";
+import type { MessageResponse, MetricsResponse } from "./types";
+
+export type { MessageResponse, MetricsResponse };
+
+export const getMetrics = async (): Promise<MetricsResponse> => {
+  return (await osmosisInstance.get("/overview/v1/metrics")).data;
+};
+
+export const getMessage = async (): Promise<MessageResponse> => {
+  return (await osmosisInstance.get("/overview/v1/message")).data;
+};
