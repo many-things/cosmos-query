7dd37921e2a4b7521716abf7bd8d9b5e0b7fb2da
Dan <ryzond6728@gmail.com>
2022-09-21 17:01:11 +0900

    Add ibc queries

---
+++ packages/core/src/apis/osmosisHistorical/ibc/index.ts
@@ -0,0 +1,81 @@
+import { osmosisInstance } from "../../../utils";
+import type {
+  IbcAllResponse,
+  IbcDestinationResponse,
+  IbcInfoResponse,
+  IbcRawResponse,
+  IbcSourceDestinationResponse,
+  IbcSourceResponse,
+} from "./types";
+
+export type {
+  IbcAllResponse,
+  IbcDestinationResponse,
+  IbcInfoResponse,
+  IbcRawResponse,
+  IbcSourceDestinationResponse,
+  IbcSourceResponse,
+};
+
+export const getIbcInfo = async (): Promise<IbcInfoResponse> => {
+  return (await osmosisInstance.get("/ibc/v1/info")).data;
+};
+
+export const getIbcAll = async ({
+  dex,
+}: {
+  dex?: string;
+}): Promise<IbcAllResponse> => {
+  return (await osmosisInstance.get("/ibc/v1/all", { params: { dex } })).data;
+};
+
+export const getIbcSourceDestination = async ({
+  source,
+  destination,
+  minutes_trigger,
+}: {
+  source: string;
+  destination: string;
+  minutes_trigger: number;
+}): Promise<IbcSourceDestinationResponse> => {
+  return (
+    await osmosisInstance.get(
+      `/ibc/v1/source/${source}/destination${destination}`,
+      {
+        params: { minutes_trigger },
+      }
+    )
+  ).data;
+};
+
+export const getIbcSource = async ({
+  source,
+  minutes_trigger,
+}: {
+  source: string;
+  minutes_trigger: number;
+}): Promise<IbcSourceResponse> => {
+  return (
+    await osmosisInstance.get(`/ibc/v1/source/${source}`, {
+      params: { minutes_trigger },
+    })
+  ).data;
+};
+
+export const getIbcDestination = async ({
+  destination,
+  minutes_trigger,
+}: {
+  destination: string;
+  minutes_trigger: number;
+}): Promise<IbcDestinationResponse> => {
+  return (
+    await osmosisInstance.get(`/ibc/v1/destination/${destination}`, {
+      params: { minutes_trigger },
+    })
+  ).data;
+};
+
+export const getIbcRaw = async (): Promise<IbcRawResponse> => {
+  return (await osmosisInstance.get("/ibc/v1/raw")).data;
+};
---
+++ packages/core/src/apis/osmosisHistorical/ibc/types.ts
@@ -0,0 +1,42 @@
+interface IbcInfo {
+  source: string;
+  destination: string;
+  channel_id: string;
+  token_symbol: string;
+  token_name: string;
+}
+
+interface IbcAll extends IbcInfo {
+  token_liquidity: number;
+  last_tx: string;
+  size_queue: number;
+  duration_minutes: number;
+}
+
+interface IbcSource extends IbcInfo {
+  last_tx: string;
+  size_queue: number;
+  duration_minutes: number;
+}
+
+export type IbcInfoResponse = IbcInfo[];
+
+export type IbcAllResponse = IbcAll[];
+
+export type IbcSourceDestinationResponse = IbcSource[];
+
+export type IbcSourceResponse = IbcSource[];
+
+export type IbcDestinationResponse = IbcSource[];
+
+export type IbcRawResponse = Record<
+  string,
+  {
+    name: string;
+    token_name: string;
+    last_tx: string;
+    counter: number;
+    size_queue: number;
+    is_trigger: boolean;
+  }
+>[];
