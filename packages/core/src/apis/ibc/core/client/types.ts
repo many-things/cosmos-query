901da2ad45ea01b51a5e3ddecadaf497226bb6f4
KIM, WOOJUNG <gnujoow@users.noreply.github.com>
2022-09-23 00:50:38 +0900

    Update packages/core/src/apis/ibc/core/client/types.ts

--- packages/core/src/apis/ibc/core/client/types.ts
+++ packages/core/src/apis/ibc/core/client/types.ts
@@ -1,5 +1,5 @@
 import type { PaginationResponse } from "../../../../types";
-import { RevisionHeight } from "../../../../types/common";
+import type { RevisionHeight } from "../../../../types/common";
 
 export interface ClientStatesResponse {
   client_states: {
