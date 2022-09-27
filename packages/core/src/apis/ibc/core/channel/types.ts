5d420e19f367d7b82a3abf9b1ea7099cb69f84a8
KimDongwu <hugo@alphaworks.xyz>
2022-09-27 22:36:52 +0900

    Change `ChannelClientStateResponse`

--- packages/core/src/apis/ibc/core/channel/types.ts
+++ packages/core/src/apis/ibc/core/channel/types.ts
@@ -41,8 +41,45 @@ export interface ChannelClientStateResponse {
   identified_client_state: {
     client_id: string;
     client_state: {
-      type_url: string;
-      value: string;
+      "@type": string;
+      chain_id: string;
+      trust_level: {
+        numerator: string;
+        denominator: string;
+      };
+      trusting_period: string;
+      unbonding_period: string;
+      max_clock_drift: string;
+      frozen_height: {
+        revision_number: string;
+        revision_height: string;
+      };
+      latest_height: {
+        revision_number: string;
+        revision_height: string;
+      };
+      proof_specs: {
+        leaf_spec: {
+          hash: string;
+          prehash_key: string;
+          prehash_value: string;
+          length: string;
+          prefix: string;
+        };
+        inner_spec: {
+          child_order: number[];
+          child_size: number;
+          min_prefix_length: number;
+          max_prefix_length: number;
+          empty_child: null | unknown;
+          hash: string;
+        };
+        max_depth: number;
+        min_depth: number;
+      }[];
+      upgrade_path: string[];
+      allow_update_after_expiry: boolean;
+      allow_update_after_misbehaviour: boolean;
     };
   };
   proof: string;
